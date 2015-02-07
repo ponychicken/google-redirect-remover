6299e3b54780e41291cd00d6dad9c7b613e583bc
Leo Koppelkamm <hello@leo-koppelkamm.de>
2015-02-07 15:59:28 +0100

    Adapt to changes

--- src/inject/inject.js
+++ src/inject/inject.js
@@ -1,15 +1,23 @@
 // Google
 (function() {
+  "use strict";
   var googRegex = /https?.\/\/.+google[^\/]*/gi;
   if (document.location.href.match(googRegex)) {
-                   
     var scrubLinks = function() {
       var els = document.getElementsByTagName('a');
       for(var i=0, il=els.length; i<il; i++) {
         var el = els[i];
+        
+        // Old school redirects
         var ni = el.attributes.getNamedItem('onmousedown');
-        if (ni && ni.textContent.substring(0, 10) === "return rwt") {
+        if (ni && ni.value.substring(0, 10) === "return rwt") {
           el.attributes.removeNamedItem('onmousedown');
+        }
+        
+        // New school redirects
+        ni = el.dataset.href;
+        if (typeof ni !== "undefined") {
+          el.href = ni.value;
         }
       }
     };
@@ -19,5 +27,15 @@
       if (timeoutId) clearTimeout(timeoutId);
       timeoutId = setTimeout(scrubLinks, 1000);
     }, false);
+    
+    /*
+    * Disable the url rewrite function
+    */
+    Object.defineProperty(window, 'rwt', {
+        value: function() { return true; },
+        writable: false, // set the property to read-only
+        configurable: false
+    });
+    
   }
 })();
